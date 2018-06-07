67bf96a3b9d30ad1e31533f29da37437236d5ef5
Simon Chuu <simonorj@outlook.com>
2018-06-07 01:28:13 -0400

    Gradle + Setup project
    ... wait, this just got a whole new setup..?

---
+++ src/main/java/com/arcaneminecraft/log/ArcaneLog.java
@@ -0,0 +1,9 @@
+package com.arcaneminecraft.log;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArcaneLog extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
